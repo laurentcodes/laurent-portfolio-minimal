c507ee904fecbb17c67e0be46e48e0c81d65e529
Saint Laurent <stlaurentcodes@gmail.com>
2024-04-11 14:39:47 +0100

    fix: update og image

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -6,11 +6,17 @@ export function GET(request: Request) {
 
 	return new ImageResponse(
 		(
-			<div tw='flex flex-col w-full h-full items-center justify-center bg-white'>
+			<div tw='flex flex-col w-full h-full items-center justify-center bg-black'>
 				<div tw='flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8'>
-					<h2 tw='flex flex-col text-4xl font-bold tracking-tight text-left'>
+					<h1 className='text-4xl font-bold tracking-tight text-white font-bold'>
+						{title}
+					</h1>
+
+					<h2 tw='flex flex-col text-4xl font-bold tracking-tight text-left text-white font-bold'>
 						{title}
 					</h2>
+
+					<img src='../og-memoji.svg' alt="Laurent's Memoji" />
 				</div>
 			</div>
 		),
