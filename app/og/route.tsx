bbc21104d173b33ffddde3e831c6254dfccccc7b
Saint Laurent <stlaurentcodes@gmail.com>
2024-04-11 15:15:14 +0100

    fix: update og image

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -5,20 +5,21 @@ import OGMemoji from '../og-memoji.svg';
 export function GET(request: Request) {
 	let url = new URL(request.url);
 	let title = url.searchParams.get('title') || 'Laurent. 👋';
+	let description = url.searchParams.get('description') || '';
 
 	return new ImageResponse(
 		(
 			<div tw='flex flex-col w-full h-full justify-center bg-black'>
 				<div tw='flex flex-col w-full py-12 px-4 justify-between p-8'>
-					<h1 className='text-4xl font-bold tracking-tight text-white font-bold'>
-						<span className='text-white font-bold'>Laurent.</span> 👋
+					<h1 tw='flex flex-col text-4xl font-bold tracking-tight text-left text-white font-bold'>
+						Laurent.
 					</h1>
 
 					<h2 tw='flex flex-col text-4xl font-bold tracking-tight text-left text-white font-bold'>
 						{title}
 					</h2>
 
-					<OGMemoji />
+					<p tw='text-lg text-white'>{description}</p>
 				</div>
 			</div>
 		),
