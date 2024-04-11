6fcd7bda2519092124885f6045e856250758d36d
Saint Laurent <stlaurentcodes@gmail.com>
2024-04-11 20:31:19 +0100

    fix: remove description from og image

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -3,21 +3,18 @@ import { ImageResponse } from 'next/og';
 export function GET(request: Request) {
 	let url = new URL(request.url);
 	let title = url.searchParams.get('title') || 'Laurent. 👋';
-	let description = url.searchParams.get('description') || '';
 
 	return new ImageResponse(
 		(
 			<div className='flex flex-col w-full h-full justify-center bg-black'>
 				<div className='flex flex-col w-full py-12 px-4 justify-between p-8'>
 					<h1 className='flex flex-col text-4xl font-bold tracking-tight text-left text-white font-bold'>
-						Laurent. <span>👋</span>
+						Laurent. 👋
 					</h1>
 
 					<h2 className='flex flex-col text-4xl font-bold tracking-tight text-left text-white font-bold'>
 						{title}
 					</h2>
-
-					<p className='text-lg text-white'>{description}</p>
 				</div>
 			</div>
 		),
