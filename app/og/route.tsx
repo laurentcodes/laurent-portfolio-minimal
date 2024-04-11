f0b7de1dbc18504882109f16609a1469c3248da5
Saint Laurent <stlaurentcodes@gmail.com>
2024-04-11 21:29:34 +0100

    fix: fix og image styling

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -1,6 +1,6 @@
 import { ImageResponse } from 'next/og';
 
-export function GET(request: Request) {
+export async function GET(request: Request) {
 	let url = new URL(request.url);
 	let title = url.searchParams.get('title') || 'Laurent. 👋';
 	let description =
@@ -9,17 +9,20 @@ export function GET(request: Request) {
 	return new ImageResponse(
 		(
 			<div tw='flex flex-col w-full h-full justify-center bg-black'>
-				<div tw='flex flex-col w-full h-full p-12 justify-between'>
-					<h1 tw='text-4xl font-bold tracking-tight text-white'>
+				<div tw='flex flex-col w-full h-full px-[200px] justify-center'>
+					<h1 tw='text-4xl font-bold tracking-tight text-white mb-12'>
 						Laurent.
 						<span role='img' aria-label='wave' tw='ml-2'>
 							👋
 						</span>
 					</h1>
 
-					<h2 tw='text-4xl font-bold tracking-tight text-white'>{title}</h2>
+					<div tw='flex flex-col'>
+						<h2 tw='text-5xl font-bold tracking-tight text-white'>{title}</h2>
+						<p tw='text-lg text-white'>{description}</p>
+					</div>
 
-					<p tw='text-lg text-white'>{description}</p>
+					<p tw='text-5xl'>💻</p>
 				</div>
 			</div>
 		),
